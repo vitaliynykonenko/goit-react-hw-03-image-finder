787b4523072773fd6222eb0b811d7f621d87aa8a
Vitaliy Nykonenko <vnykonenko@ukr.net>
2022-10-23 22:22:46 +0300

    Для здачі ДЗ 3

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,42 @@
+import { Component } from 'react';
+import { createPortal } from 'react-dom';
+import PropTypes from 'prop-types';
+import { Overlay, ModalStyled } from './Modal.styled';
+
+const modalRoot = document.querySelector('#modal-root');
+
+export class Modal extends Component {
+  static propTypes = {
+      onClose: PropTypes.func.isRequired,
+    children: PropTypes.element,
+  };
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+
+  handleBackdropClick = e => {
+    if (e.currentTarget === e.target) {
+      this.props.onClose();
+    }
+  };
+
+  render() {
+    return createPortal(
+      <Overlay onClick={this.handleBackdropClick}>
+        <ModalStyled>{this.props.children}</ModalStyled>
+      </Overlay>,
+      modalRoot
+    );
+  }
+}
